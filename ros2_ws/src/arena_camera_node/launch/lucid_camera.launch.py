9f9d18c63fed9b9b309a969458a5c9285d76f3d8
Russ Webber <russ.webber@greenroomrobotics.com>
2022-06-08 17:05:19 +1000

    feat: wait for ptp sync before starting streaming

--- ros2_ws/src/arena_camera_node/launch/lucid_camera.launch.py
+++ ros2_ws/src/arena_camera_node/launch/lucid_camera.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
                 # 'frame_rate': 10.0,
                 # 'test_pattern': 'Pattern3',
                 'trigger_mode': False,
-                # "ptp": True,
+                "ptp": True,
                 'frame_id': "/camera",
                 'qos_history': 'keep_last',
                 'qos_history_depth': 1,
